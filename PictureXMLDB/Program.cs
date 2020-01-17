084945cd14d9f5a1fecd9c4fedc5bcf89c729d5a
Philip Dando <flint276@hotmail.com>
2020-01-17 23:28:36 +0000

    Add project files.

---
+++ PictureXMLDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PictureXMLDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
